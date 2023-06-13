e7672af6e176bb61b27980a6fda979aa5d0d941a
Vian Maulana Ramadhan <vianmaulana540@gmail.com>
2023-06-13 11:12:57 +0700

    create halaman tentang kami (about)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.front')
+
+@section('title', 'Tentang Kami')
+    
+@section('content')
+{{-- Banner --}}
+<div class="banner bg-pendana">
+    <div class="container">
+        <h2 class="fa-2x text-white">Tentang Kami</h2>
+    </div>
+</div>
+
+{{-- Tentang Kami --}}
+<div class="tentang-kami bg-white">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-12 text-justify">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio cumque exercitationem at? Corporis,
+                    unde? Laboriosam, officia? Consectetur porro nihil, expedita tempore facere aperiam officiis
+                    molestias odio laborum!</p>
+                <p>Facilis laborum provident quos delectus labore necessitatibus aperiam quo
+                    enim eius? Optio enim dicta, sequi quibusdam cum vero eius, tempora voluptatum nisi quaerat
+                    doloremque repellendus placeat nulla ipsum perferendis quasi itaque asperiores eum sint
+                    voluptatibus, est nostrum.</p> 
+                <p>Enim praesentium inventore provident optio eligendi quos totam adipisci
+                    accusamus vero nulla cum, dolorem esse ad quisquam repellat earum. Perferendis at inventore suscipit
+                    maxime, beatae sint expedita sapiente facilis quis accusamus eligendi nobis, atque dolorum
+                    molestiae.</p>
+
+                <h4 class="mt-4">Krikit dan Saran</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam omnis totam facere debitis 
+                    ducimus atque non vitae odit voluptatem dicta!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
